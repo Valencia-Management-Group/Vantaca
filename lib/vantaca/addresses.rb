1b87da87ab02bf66a8ae5d071d5e51fbfad59eaf
Steven Hoffman <git@fustrate.com>
2020-11-11 22:19:38 -0800

    code cleanup
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/vantaca/addresses.rb
+++ lib/vantaca/addresses.rb
@@ -7,14 +7,18 @@ module Vantaca
   module Addresses
     def create_address(homeowner_id, address_attributes)
       post('/Write/AddressCreate', address_attributes.merge(hoid: homeowner_id))
+
+      true
     end
 
     def update_address(address_id, address_attributes)
       post('/Write/AddressUpdate', address_attributes.merge(addrID: address_id))
+
+      true
     end
 
     def delete_address(address_id)
-      response = post('/Write/AddressDestroy', addrID: address_id)
+      post('/Write/AddressDestroy', addrID: address_id)
 
       true
     end
--- spec/vantaca/configuration_spec.rb
+++ spec/vantaca/configuration_spec.rb
@@ -10,27 +10,27 @@ RSpec.describe Vantaca::Configuration do
     let(:invalid_config_params) { ['Ü', nil, '', 'A/b/3!', 'äbcdef0123456789äbcdef0123456789'] }
 
     it 'creates a new client with an alphan' do
-      expect  { configure!(company: 'Vantaca', login: 'admin', password: 'abc123') }
+      expect { configure!(company: 'Vantaca', login: 'admin', password: 'abc123') }
         .to_not raise_exception
     end
 
     it 'fails when the company is not alphanumeric characters' do
       invalid_config_params.each do |param|
-        expect  { configure!(company: param, login: 'admin', password: 'abc123') }
+        expect { configure!(company: param, login: 'admin', password: 'abc123') }
           .to raise_exception ArgumentError
       end
     end
 
     it 'fails when the login is not alphanumeric characters' do
       invalid_config_params.each do |param|
-        expect  { configure!(company: 'Vantaca', login: param, password: 'abc123') }
+        expect { configure!(company: 'Vantaca', login: param, password: 'abc123') }
           .to raise_exception ArgumentError
       end
     end
 
     it 'fails when the password is not alphanumeric characters' do
       invalid_config_params.each do |param|
-        expect  { configure!(company: 'Vantaca', login: 'admin', password: param) }
+        expect { configure!(company: 'Vantaca', login: 'admin', password: param) }
           .to raise_exception ArgumentError
       end
     end
